6a13a9ddc6245767e5dd5819270691a08372dc7e
JFrancis <jtf10@student.le.ac.uk>
2021-12-01 15:19:55 +0000

    Updates keyboardTest.py
    Supports multiple key pressing now. Basic Flight time also working

--- Code/Test code/keyboardTest.py
+++ Code/Test code/keyboardTest.py
@@ -6,47 +6,95 @@ from numpy import add
 from numpy.lib.function_base import append
 
 # Record events until 'esc' is pressed and then plays them
-recorded = keyboard.record(until='esc')
-print(recorded)
-rawKeys = []
-holdTimeArray = []
-avgHoldTimeArray = []
-count = 0
-for record in recorded:
-    print(record.name, record.time, record.event_type, count)
-    count += 1
-    rawKeys.append([record.name, record.time, record.event_type])
+def record():
+    recorded = keyboard.record(until='esc')
+    print(recorded)
+    rawKeys = []
+    count = 0
+    for record in recorded:
+        print(record.name, record.time, record.event_type, count)
+        count += 1
+        rawKeys.append([record.name, record.time, record.event_type])
+    return rawKeys
     
-print("Raw Data:")
-for x in rawKeys:
-    print(x)
+# Calculate pairs and hold time
+def pairs(rawKeys):
+    holdTimeArray = []
+    for i in range(len(rawKeys)):
+        try:
+            if (rawKeys[i][2] == 'down' and rawKeys[i+1][2] == 'up'):
+                holdTime = rawKeys[i+1][1] - rawKeys[i][1]
+                found = False
+                for c in holdTimeArray:
+                    if c[0] == rawKeys[i][0]:
+                        c.append(holdTime)
+                        found = True
+                if found == False:
+                    holdTimeArray.append([rawKeys[i][0], holdTime])
+            else:
+                for x in range(len(rawKeys)):
+                    if (rawKeys[x][0].lower() == rawKeys[i][0].lower() and rawKeys[x][2] == 'up' and rawKeys[i][2] == 'down'):
+                        holdTime = rawKeys[x][1] - rawKeys[i][1]
+                        found = False
+                        for c in holdTimeArray:
+                            if c[0] == rawKeys[i][0]:
+                                c.append(holdTime)
+                                found = True
+                        if found == False:
+                            holdTimeArray.append([rawKeys[i][0], holdTime])        
+        except IndexError:
+            pass;
+    return holdTimeArray
 
-print("Pairs:")
-for i in range(len(rawKeys)):
-    try:
-        if (rawKeys[i][2] == 'down' and rawKeys[i+1][2] == 'up'):
-            holdTime = rawKeys[i+1][1] - rawKeys[i][1]
-            print("Key: " + rawKeys[i][0] + " Holdtime = " + str(holdTime))
-            found = False
-            for c in holdTimeArray:
-                if c[0] == rawKeys[i][0]:
-                    c.append(holdTime)
-                    found = True
-            if found == False:
-                holdTimeArray.append([rawKeys[i][0], holdTime])
-    except IndexError:
-        pass;
+# Calculate average hold time
+def avgHoldTime(holdArray):
+    avgHoldTimeArray = []
+    for b in holdArray:
+        if (len(b)>2):
+            addInt = 0.0
+            for i in range(1,len(b)):
+                addInt += b[i]
+            avgHoldTime = float(addInt) / float(len(b))
+            avgHoldTimeArray.append([b[0], avgHoldTime])
+        else:
+            avgHoldTimeArray.append([b[0], b[1]])
+    return avgHoldTimeArray
 
-print("Hold Times for each key:")
-for b in holdTimeArray:
-    print(b)
-    if (len(b)>2):
-        addInt = 0.0
-        for i in range(1,len(b)):
-            addInt += b[i]
-        avgHoldTime = float(addInt) / float(len(b))
-        avgHoldTimeArray.append([b[0], avgHoldTime])
+# Calculate float time
+def floatTime(rawData):
+    for x in range(len(rawData)):
+        if (rawData[x][2] == 'up'):
+            for i in range(x, len(rawData)):
+                if (rawData[i][2] == 'down'):
+                    flightime = rawData[i][1] - rawData[x][1]
+                    print(rawData[x][0] + " " +  rawData[i][0] + " " + str(flightime))
+                    break;
+                    
+                    
+            
+    
 
-print("Average Hold Times for each key")
-for q in avgHoldTimeArray:
-    print("Key: " + q[0] + " Average hold time: " + str(q[1]))
+   
+if __name__ == "__main__":
+    rawData = record()
+    print("Raw Data:")
+    for x in rawData:
+        print(x)
+    
+    holdTimes = pairs(rawData)
+    print("Pairs")
+    for y in holdTimes:
+        print("Key: " + y[0] + " Holdtime = " + str(y[1]))
+        
+    avgHoldTimes = avgHoldTime(holdTimes)
+    print("Average Hold Times for each key")
+    for q in avgHoldTimes:
+        print("Key: " + q[0] + " Average hold time: " + str(q[1]))
+        
+    floatTime(rawData)
+    
+    
+    
+    
+
+
